a13f69d403e6aed9020eea731142a0e5175d90cf
Christian Bode <christianbode97@web.de>
2024-11-18 12:32:23 +0100

    changed to singular vector store with doc_id+start_index in metadata

--- software_environment.py
+++ software_environment.py
@@ -22,6 +22,7 @@ class Software_environment():
     def __init__(self):
         load_dotenv()
         self.COHERE_API_KEY = os.getenv('COHERE_API_KEY')
+        self.create_vectorstore()
         self.load_docs()
         if len(self.documents) > 0:
             self.current_document_id = 1
@@ -33,28 +34,34 @@ class Software_environment():
                 cas = cassis.load_cas_from_json(f)
                 self.documents[self.next_id] = cas
 
-                # Load the document, split it into chunks, embed each chunk and load it into the vector store.
-                # text_splitter = CharacterTextSplitter(
-                #     chunk_size=1, chunk_overlap=0, separator="\n")
-                text_splitter = RecursiveCharacterTextSplitter(
-                    chunk_size=1000, chunk_overlap=200, add_start_index=True
-                )
-                documents = text_splitter.split_text(cas.sofa_string)
-                embeddings = CohereEmbeddings(
-                    cohere_api_key=self.COHERE_API_KEY, model="embed-english-v3.0")
-                self.vector_stores[self.next_id] = Chroma.from_texts(
-                    documents, embeddings)
+                # add to vector store
+                self.add_cas_to_vectorstore(cas, self.next_id)
 
                 self.next_id += 1
+
+    def create_vectorstore(self):
+        embeddings = CohereEmbeddings(
+            cohere_api_key=self.COHERE_API_KEY, model="embed-english-v3.0")
+        self.vector_store = Chroma(
+            collection_name="INCEpTION",
+            embedding_function=embeddings,
+        )
+
+    def add_cas_to_vectorstore(self, cas, doc_id):
+        # Load the document, split it into chunks, embed each chunk and load it into the vector store.
+        # text_splitter = CharacterTextSplitter(
+        #     chunk_size=1, chunk_overlap=0, separator="\n")
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200, add_start_index=True
+        )
+        documents = text_splitter.create_documents([cas.sofa_string], metadatas=[{"doc_id": doc_id}])
+        self.vector_store.add_documents(documents)
 
     def get_documenttext_by_id(self, id: int):
         return self.documents[id].sofa_string
 
     def get_document_by_id(self, id: int):
         return self.documents[id]
-
-    def get_vectorstore_by_id(self, id: int):
-        return self.vector_stores[id]
 
     def get_documents(self):
         return self.documents
@@ -71,8 +78,8 @@ class Software_environment():
     def get_current_documenttext(self):
         return self.documents[self.current_document_id].sofa_string
 
-    def get_current_vectorstore(self):
-        return self.vector_stores[self.current_document_id]
+    def get_vectorstore(self):
+        return self.vector_store
 
     def update_current_document(self, cas):
         self.documents[self.current_document_id] = cas
