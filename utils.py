ad44f589fffddc77c82906e9d72ba8dd289debfb
Christian Bode <christianbode97@web.de>
2025-04-26 10:14:42 +0200

    fixed reasoning model parse. Fixed bug in heuristic in DAG validation

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import re
 import logging
+from collections import defaultdict
 
 from os import listdir, getcwd
 from os.path import join
@@ -32,9 +33,11 @@ def remove_file_ending(_str):
 
 
 def parse_deepseek_response(_str):
-    _str == (re.split(r'think>', _str)[1]).strip()
+    _lst = re.split(r'think>', _str)
+    if len(_lst) > 2:
+        _str = _lst[2].strip()
     if r'\`\`\`' in _str:
-        _str == (re.split(r'\`\`\`', _str)[1]).strip()
+        _str = (re.split(r'\`\`\`', _str)[1]).strip()
     return _str
 
 
@@ -56,43 +59,75 @@ def load_dag(dag: dict) -> dict:
         get_layer_and_feature: ['annotate'],
         classify_span: ['annotate'],
         annotate: ['respond'],
-        respond: ['<END>']
+        respond: []
     }"""
     # tasks in values that are not present as a key only have incoming edges
-    # thus we add END tokens to them
+    # still create an edge for them to an empty list
     a = list(dag.values())
     tasks = [item for sublist in a for item in sublist]
     for task in set(tasks):
         if not task in dag.keys():
-            # add end token
-            dag[task] = ["<END>"]
+            dag[task] = []
     return dag
 
 
-def eval_functions_dag(functions: list, dag: dict[str, list]):
-    """Evaluates an ordered list of functions according to a dag
-    returns validity and number of unnecessary functions
-    dag looks like:
+def create_children_dict(dag: dict[str, list[str]]):
+    """returns a dictionary where the keys are the nodes and the values are the list of their children
+    input:
     {
         root: [getscope, getlayer],
         getscope: [classify],
         getlayer: [annotate],
         classify: [annotate],
         annotate: [respond],
-        respond: [<END>]
+        respond: []
+    }
+    output:
+    {
+        'get_scope': [],
+        'get_layer': [], 
+        'classify': ['get_scope'], 
+        'annotate': ['get_layer', 'classify'], 
+        'respond': ['annotate']
+    }"""
+    result_dict = defaultdict(list)
+    for k, v in dag.items():
+        if k == "root":
+            for node in v:
+                result_dict[node] = []
+        else:
+            for node in v:
+                result_dict[node].append(k)
+    return (result_dict)
+
+
+def eval_functions_dag(functions: list[str], dag: dict[str, list[str]]):
+    """Evaluates an ordered list of functions according to a dag
+    returns validity and number of unnecessary functions
+    DAG looks like:
+    {
+        root: [getscope, getlayer],
+        getscope: [classify],
+        getlayer: [annotate],
+        classify: [annotate],
+        annotate: [respond],
+        respond: []
     }"""
     current_nodes = dag["root"]
+    dict_children = create_children_dict(dag)
     nb_unused_funcs = 0
     for func in functions:
-        if not func in current_nodes:
-            # function was "unnecessary", didN't move pointer further along in dag
+        if not func in current_nodes or dict_children[func] != []:
+            # function was "unnecessary", did not move pointer further along in dag
             nb_unused_funcs += 1
         else:
             # function moved pointer further ahead
             current_nodes = [el for el in current_nodes if el != func]  # remove all funcs
             current_nodes.extend(dag[func])
-    valid = (current_nodes == ["<END>"])
-    return valid, nb_unused_funcs
+            for new_leaf_node in dag[func]:
+                dict_children[new_leaf_node].remove(func)
+    is_valid = (current_nodes == [])
+    return is_valid, nb_unused_funcs
 
 
 def flatten_dag(dag) -> str:
@@ -103,7 +138,7 @@ def flatten_dag(dag) -> str:
         getlayer: [annotate],
         classify: [annotate],
         annotate: [respond],
-        respond: [<END>]
+        respond: []
     }
     ->
     'getscope -> classify, getlayer -> annotate -> respond'
@@ -111,7 +146,7 @@ def flatten_dag(dag) -> str:
     # get last nodes (incoming edge but no outgoing)
     last_nodes = []
     for k, v in dag.items():
-        if v == ["<END>"]:
+        if v == []:
             last_nodes.append(k)
     # go backwards
     lst = [last_nodes]
@@ -133,6 +168,33 @@ def flatten_dag(dag) -> str:
     # getscope -> classify, getlayer -> annotate -> respond
     _str = ' -> '.join([', '.join(sublist) for sublist in lst])
     return _str
+
+
+def detect_functions_parampred(funcs_compound):
+    compound_map = {
+        "annotate": ["get_layer_and_feature", "annotate"],
+        "classify_span": ["get_scope", "classify_span"],
+        "highlight": ["highlight"],
+        "summarize_document": ["get_scope", "summarize_document"],
+        "search_annotations": ["search_annotations"],
+        "search_context": ["search_context"],
+        "respond": ["respond"],
+    }
+
+    return [func for funcs_atomic in funcs_compound for func in compound_map[funcs_atomic]]
+
+
+def detect_functions_compound(funcs_compound):
+    compound_map = {
+        "annotate": ["get_scope", "get_layer_and_feature", "classify_span", "annotate"],
+        "highlight": ["get_scope", "classify_span", "highlight"],
+        "summarize_document": ["get_scope", "summarize_document"],
+        "search_annotations": ["search_annotations"],
+        "search_context": ["search_context"],
+        "respond": ["respond"],
+    }
+
+    return [func for funcs_atomic in funcs_compound for func in compound_map[funcs_atomic]]
 
 
 class ParsingException:
@@ -157,7 +219,21 @@ if __name__ == "__main__":
         "annotate": ['respond']
     }
     d = load_dag(d)
-    print(d)
-    print(eval_functions_dag(dag=d, functions=["get_scope", "search_context",
-          "get_layer_and_feature", "classify_span", "annotate", "respond"]))
-    print(flatten_dag(d))
+    # print(d)
+    print(eval_functions_dag(dag=d, functions=["get_scope",
+          "classify_span",  "annotate",  "get_layer_and_feature", "annotate", "respond"]))
+    # print(flatten_dag(d))
+
+    # lst = ["classify_span", "annotate", "respond"]
+    # a = detect_functions_parampred(lst)
+    # print(a)
+    # c = ['get_scope',  'classify_span', 'get_layer_and_feature', 'annotate', 'respond']
+    # b = {
+    #     "root": ['get_scope', 'get_layer_and_feature'],
+    #     "get_scope": ['classify_span'],
+    #     "get_layer_and_feature": ['annotate'],
+    #     "classify_span": ['annotate'],
+    #     "annotate": ['respond'],
+    #     "respond": ['<END>']
+    # }
+    # print(eval_functions_dag(c, b))
